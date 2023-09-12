278dca43f807fd2cd98e4e9e907c18f99fc55f4f
idhcbIan <idhcb.ian@gmail.com>
2023-09-12 19:20:04 -0300

    huh5

--- trabalho1/p2/p2.c
+++ trabalho1/p2/p2.c
@@ -9,25 +9,70 @@
 ╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰╯
 */
 #include<stdio.h>
+#include<stdbool.h>
 
 //Criando uma funcao que Printa um vetor(para visualisacao) //-----------
-void vector_print(int v[], int size){
-    for(int i = 0; i < size; i++){
+void vector_print(int size, int v[size]) {
+    for(int i = 0; i < size; i++) {
         printf("%d ", v[i]);
+
     }
     printf("\n");
 }
 
-// Definindo o Vetor //-----------
-int vector[(10)] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
- 
+
+//Criando uma funcao que busca o vetor //-----------
+void vector_find(int size, int v[size], int find) {
+  bool did_y_find = false;
+    for(int i = 0; i < size; i++) {
+        if (v[i] == find){
+        did_y_find = true;
+        }
+
+    }
+  if (did_y_find == true){
+    printf("SIM\n");
+  }else {
+    printf("NAO\n");
+  }
+}
+
+int main() {
+    //Pegando tamanho
+    int size;
+    scanf("%d", &size);
+    
+    //Pegando vetor
+    int vector[size];
+
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &vector[i]);
+    }
+
+    
+    //Pegando numero de pesquisas
+    int searches;
+    scanf("%d", &searches);
+   
+
+    //Pegando pesquisas
+    int find;
+    int to_find[searches];
+
+    
+    for(int i=0; i<searches; i++){
+        scanf("%d", &find);
+        to_find[i] = find;
+    }
 
 
-int main(){
-    
-    //Descobrindo Tamanho do Vetor //-----------
-    int size = sizeof(vector) / sizeof(vector[0]); 
-    int i = size-1;
-    
+    //Buscando a lista
+    for(int i=0; i<searches; i++){
+      vector_find(size, vector, to_find[i]);
+    }
+
+
+
+
     return 0;
 }
