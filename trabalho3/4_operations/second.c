0d01edda06ff9cc997095ec4194bc93d58a04807
IdhcbIan <idhcb.ian@gmail.com>
2023-11-14 17:36:00 -0300

    14/11/17

---
+++ trabalho3/4_operations/second.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int main() {
+
+    char a[10] = '1234';
+    char b[10] = '2345';
+
+    if (a>b){
+
+    printf("%s\n",a);
+    }
+
+    return 0;
+}
